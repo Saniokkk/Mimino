03780367853ca31b82a1b8583d3aebbaf08da541
Saniokkk <kashirin.alexsandr91@gmail.com>
2023-03-05 16:04:45 +0200

    add plagin imagemin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import viteImagemin  from 'vite-plugin-imagemin';
+
 console.log(glob.sync('./src/*.html'));
 
 export default defineConfig({
@@ -12,5 +14,35 @@ export default defineConfig({
     },
     outDir: '../dist',
   },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html']),
+      viteImagemin({
+        gifsicle: {
+          optimizationLevel: 3,
+          interlaced: false,
+        },
+        optipng: {
+          optimizationLevel: 5,
+        },
+        mozjpeg: {
+          quality: 50,
+        },
+        pngquant: {
+          quality: [0.8, 0.9],
+          speed: 4,
+        },
+        svgo: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+            },
+            {
+              name: 'removeEmptyAttrs',
+              active: false,
+            },
+          ],
+        },
+      }),
+    ],
 });
